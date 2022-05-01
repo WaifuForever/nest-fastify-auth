7cc5d8dc35626063649910c2c87cd6b35546f28a
Waifu Forever <itachiut1r4@gmail.com>
2022-05-01 09:02:34 -0300

    add: e2e test

---
+++ test/mocks/user.mock.ts
@@ -0,0 +1,15 @@
+import { User } from '../../src/models/users/interfaces/user.interface';
+const acc1: User = {
+    password: 'asdkaXs@3123',
+    email: 'email@gmail.com',
+};
+const acc2: User = {
+    password: 'adasda@Sd3123',
+    email: 'email2@email.com',
+};
+
+const acc3_fake: User = {
+    password: 'adasdad3123',
+    email: 'email2@emacom',
+};
+export { acc1, acc2, acc3_fake };
