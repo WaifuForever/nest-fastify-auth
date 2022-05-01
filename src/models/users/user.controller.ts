c73bd30f168b3aa2feb8ae66ce69cb204ae62f47
Waifu Forever <itachiut1r4@gmail.com>
2022-05-01 00:17:45 -0300

    add: user model

---
+++ src/models/users/user.controller.ts
@@ -0,0 +1,61 @@
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    Query,
+    UseGuards,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { UserService } from './user.service';
+
+@Controller('Users')
+export class UserController {
+    constructor(private _service: UserService) {}
+
+    @Post()
+    @UsePipes(
+        new ValidationPipe({
+            whitelist: true,
+        }),
+    )
+    async create(@Body() User): Promise<User> {
+        return this._service.createUser(User);
+    }
+
+    @Get('find')
+    async findOne(
+        @Query('id') _id: string,
+        @Query('email') _email: string,
+    ): Promise<User | undefined> {
+        if (_email) return this._service.findByEmail(_email);
+        else if (_id) return this._service.findById(_id);
+    }
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return this._service.findAllUsers();
+    }
+
+    @UsePipes(
+        new ValidationPipe({
+            whitelist: true,
+            skipMissingProperties: true,
+        }),
+    )
+    @Put(':id')
+    async updateById(@Param('id') id: string, @Body() dto): Promise<object> {
+        return this._service.updateById(id, dto);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string): Promise<object> {
+        return this._service.deleteById(id);
+    }
+}
---
+++ src/models/users/user.entity.ts
@@ -0,0 +1,41 @@
+import * as bcrypt from 'bcrypt';
+
+import {
+    BeforeInsert,
+    Column,
+    CreateDateColumn,
+    Entity,
+    Index,
+    JoinColumn,
+    ManyToOne,
+    OneToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('user')
+export class UserEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @BeforeInsert()
+    async hashPassword() {
+        console.log(`rounds=${process.env.HASH_SALT}; hashing password...`);
+        this.password = await bcrypt.hash(
+            this.password,
+            Number(process.env.HASH_SALT),
+        );
+        console.log('password hashed...');
+    }
+    @Column({ select: false })
+    password: string;
+
+    @Index()
+    @Column({ unique: true })
+    email: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+
+    @Column({ default: 0 })
+    tokenVersion: number;
+}
